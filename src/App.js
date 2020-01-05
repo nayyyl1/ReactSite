fcdc55dcb3b3ed03329dede7a25fe4c0b1afc7c0
nayyyl1 <nayyyl990529@gmail.com>
2020-01-05 23:00:12 +0700

    try1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./image/me.png";
+import "./App.css";
+import "./Component/Home";
 
 function App() {
   return (

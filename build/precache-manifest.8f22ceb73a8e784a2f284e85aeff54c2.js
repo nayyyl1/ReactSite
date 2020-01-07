d4291b1eb26fdcb1a9787ec96f401b8f271b936c
nayyyl1 <nayyyl990529@gmail.com>
2020-01-08 00:48:41 +0700

    update

---
+++ build/precache-manifest.8f22ceb73a8e784a2f284e85aeff54c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9cb7eef605040debab0efeb95cba27",
+    "url": "/ReactSite/index.html"
+  },
+  {
+    "revision": "2f9e1c2a14f93bfea24f",
+    "url": "/ReactSite/static/css/main.eae7a823.chunk.css"
+  },
+  {
+    "revision": "2c7e6c12fd03a2b27b86",
+    "url": "/ReactSite/static/js/2.0be1d422.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactSite/static/js/2.0be1d422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f9e1c2a14f93bfea24f",
+    "url": "/ReactSite/static/js/main.ac88654d.chunk.js"
+  },
+  {
+    "revision": "e77c259bf7a82a5b6d0c",
+    "url": "/ReactSite/static/js/runtime-main.91a55068.js"
+  },
+  {
+    "revision": "5e9d77f6de25ce0f72aa7a6b80870667",
+    "url": "/ReactSite/static/media/me.5e9d77f6.png"
+  }
+]);
---
+++ src/Component/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="Main-content">
+      <div className="Intro">
+        <div>
+          <div className="Row Text-4">
+            <p>HI</p>
+            <p className="Gantan">!</p>
+          </div>
+          <div className="Row Text-4">
+            <p>THIS IS NAY</p>
+            <p className="Gantan">!</p>
+          </div>
+          <div className="Row Text-4">
+            <p>( ◔ ڼ ◔ )</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
